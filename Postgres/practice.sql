57521aba8c81bd4b38c38b37c721c3d5901dd00e
sharmachait <chait8126@gmail.com>
2024-05-10 16:15:30 +0530

    practice

--- Postgres/practice.sql
+++ Postgres/practice.sql
@@ -55,7 +55,7 @@ DROP TABLE customer;
 
 DROP TYPE sex;
 
-CREATE TABLE product_type(
+CREATE TABLE sales_person(
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
@@ -71,6 +71,67 @@ CREATE TABLE product_type(
 	sex sex_type NOT NULL
 );
 
-CREATE TABLE sales_person(
-	
+CREATE TABLE product_type(
+	id SERIAL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
 );
+
+CREATE TABLE product(
+	id SERIAL PRIMARY KEY,
+	type_id INTEGER REFERENCES product_type(id),
+	name VARCHAR(30) NOT NULL,
+	supplier VARCHAR(30) NOT NULL,
+	description TEXT NOT NULL
+);
+
+CREATE TABLE item(
+	id SERIAL PRIMARY KEY,
+	product_id INTEGER REFERENCES product(id),
+	size INTEGER NOT NULL,
+	color VARCHAR(30) NOT NULL,
+	picture VARCHAR(256) NOT NULL,
+	price NUMERIC(6,2) NOT NULL
+);
+
+CREATE TABLE sales_order(
+	id SERIAL PRIMARY KEY,
+	cust_id INTEGER REFERENCES customer(id),
+	sales_person_id INTEGER REFERENCES sales_person(id),
+	time_order_taken TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+	purchase_orer_number INTEGER NOT NULL,
+	credit_card_number VARCHAR(16) NOT NULL,
+	credit_card_exper_month SMALLINT NOT NULL,
+	credit_card_exper_day SMALLINT NOT NULL,
+	credit_card_secret_code SMALLINT NOT NULL,
+	name_on_card VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE sales_item(
+item_id INTEGER REFERENCES item(id),
+sales_order_id INTEGER REFERENCES sales_order(id),
+quantity INTEGER NOT NULL,
+discount NUMERIC(3,2) NULL DEFAULT 0,
+taxable BOOLEAN NOT NULL DEFAULT FALSE,
+sales_tax_rate NUMERIC(5,2) NOT NULL DEFAULT 0,
+id SERIAL PRIMARY KEY
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
